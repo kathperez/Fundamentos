fa823731a8a40ea1b26d2c5282e79592b7e5e94c
Katherinne Pérez <36804453+kathperez@users.noreply.github.com>
2018-06-07 17:30:16 -0600

    Class # 3

---
+++ Inclass/Semana 3/src/semana/pkg3/Semana3.java
@@ -0,0 +1,131 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package semana.pkg3;
+import java.util.*;
+import java.util.Scanner;
+
+/**
+ *
+ * @author Kath
+ */
+public class Semana3 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+ 
+        Scanner scan = new Scanner(System.in);
+        
+        
+//        System.out.println("¿Que quiere hacer?");
+//        System.out.println("Presione D= Debitar, C= Acreditar, S= Salir");
+//        int cuenta = 1000;
+//        String decision;
+//        decision = scan.next();  
+//        while (decision.compareTo("s")!=0){
+//            
+//          if (decision.compareTo("d") == 0){
+//              System.out.println("Introduzca el monto a debitar");
+//              int monto = scan.nextInt();
+//              int remanente = cuenta- monto;
+//      
+//              if (remanente>= 0){
+//                  cuenta = cuenta - monto;
+//                  System.out.println("Debito listo, quedan: $ "+ cuenta);
+//                  
+//              }else {
+//                  System.out.println("No tiene fondos suficientes. solo tienes: $ "+ cuenta);
+//              } 
+//              
+//          }else if (decision.compareTo("c") == 0){
+//               System.out.println("Introduzca el monto que desea agregar");
+//              int monto = scan.nextInt();
+//              cuenta = cuenta + monto;
+//              System.out.println("El Saldo disponible es de: $ " + cuenta);
+//          }
+//                    
+//        System.out.println("¿Que quiere hacer?");
+//        System.out.println("Presione D= Debitar, C= Acreditar, S= Salir");
+//        decision = scan.next();
+//    }
+//        System.out.println("Saldo final: $ "+ cuenta);
+//    }
+
+//////////////////////////////////////////////////////////////////////////////////////////////////
+
+// Ejercicio #1
+
+// int x = 1;
+//
+//  int i = 0;
+//  int suma=0;
+//  double promedio;
+//  while (x != 0 ){
+//    System.out.println("Digite un numero");   
+//    x = scan.nextInt();
+//    suma = suma + x;
+//    i = i +1;
+//  }
+//  promedio = suma/i;
+//  System.out.println("El promedio de los numeros es: "+promedio);
+//}
+/////////////////////////////////////////////////////////////////////////////////////////////////////////    
+
+
+    //Ejercicio #2
+//    
+//    
+//   int a = 0 , i, n;
+//   System.out.println("Ingrese numero");
+//   int num= scan.nextInt();
+//   for (i=1; i <(num+1);i++){
+//    if (num%i==0){
+//           a++;
+//       }
+//   }
+//    if (a!=2){
+//       System.out.println("No es primo");
+//       
+//    }else {System.out.println("Si es primo");
+//    }
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////  
+
+
+// Ejercicio #3
+//String palabras;
+//System.out.println("Escribe una frase: ");
+//palabras = scan.nextLine();
+//StringTokenizer a = new StringTokenizer(palabras);
+//System.out.println("El numero de palabrases: "+a.countTokens());
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+//Ejercicio#4
+//int size;
+//System.out.println("Inserta el tamaño del cuadrado: ");
+//size= scan.nextInt();
+//for (int i=1; i <= size;i++ ){
+//    for(int j=1; j<=size; j++){
+//     if(j==1 || j== size || i==1 || i== size){
+//     System.out.print("* ");
+//     }else{
+//     System.out.print("  ");
+//     }
+//    }
+//    System.out.println();
+//}
+
+
+
+
+
+
+
+    }}
+
+
+    
+
