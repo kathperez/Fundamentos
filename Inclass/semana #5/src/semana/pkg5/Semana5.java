e23efae4fa900d6da796675c65211feefd3524f3
Katherinne Pérez <36804453+kathperez@users.noreply.github.com>
2018-06-11 16:33:08 -0600

    Semana #5 06-11
    Primer ejercicio

---
+++ Inclass/semana #5/src/semana/pkg5/Semana5.java
@@ -0,0 +1,61 @@
+
+package semana.pkg5;
+
+import java.util.Random;
+import java.util.Scanner;
+
+public class Semana5 {
+
+   
+    public static void main(String[] args) {
+      
+//        Random ran = new Random();
+        Scanner scan = new Scanner(System.in);
+//        
+//        int numAleatorio = ran.nextInt(100);
+//        System.out.println("Ingrese un numero aleatorio: ");
+//        int numAdivinar = scan.nextInt();
+//        
+//  
+//        while(numAdivinar != numAleatorio) {
+//            if (numAdivinar > numAleatorio){
+//                System.out.println("El numero es mayor");
+//                
+//            }else if (numAdivinar < numAleatorio){
+//                System.out.println("El numero es menor");
+//            }
+//           System.out.println("Ingrese un numero aleatorio: ");
+//           numAdivinar = scan.nextInt(); 
+//        }
+//        
+//        System.out.println(numAleatorio);
+        
+        
+////////////////////////////////////////////////////////////////////////
+   System.out.println("Ingrese el monto que desea desglosar: ");
+   int monto = scan.nextInt();
+//   //billetes
+//    int a= 20000;
+//    int b= 10000;
+//    int c= 5000;
+//    int d= 2000;
+//    int e= 1000;
+//    //monedas
+//    int f= 500;
+//    int g= 100;
+//    
+    
+    
+    }
+    
+        
+    }
+        
+       
+        
+        
+        
+        
+        
+    
+    
